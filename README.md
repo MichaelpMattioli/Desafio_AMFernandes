0bb96fb78761458a70a60114ff6539f90467e7fe
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-27 16:54:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Desafio_AMFernandes
+ Desafio proposto pela AMFernandes
---
+++ src/br/desafio/Main.java
@@ -0,0 +1,8 @@
+package br.desafio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ola mundo!");
+    }
+}
