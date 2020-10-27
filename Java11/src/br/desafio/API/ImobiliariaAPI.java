cc1353ec0be1dd0ab096fe67c4653be029df2613
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-27 17:32:54 -0300

    Criação da requisição do JSON
    função exclusiva para fazer a requisição do JSON

--- Java11/src/br/desafio/API/ImobiliariaAPI.java
+++ Java11/src/br/desafio/API/ImobiliariaAPI.java
@@ -1,5 +1,23 @@
 package br.desafio.API;
 
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ImobiliariaAPI {
-    
+    public JSONObject imoveisJsonObject() throws Exception{
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://api.estagio.amfernandes.com.br/imoveis"))
+                .build();
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+        JSONObject jsonObject = new JSONObject(response.body());
+
+        return jsonObject;
+    }
 }
