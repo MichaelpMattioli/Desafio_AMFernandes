4d3834263dc3e04769e4a6d405c0ad88dbafe281
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-28 00:59:24 -0300

    Refatorando a classe imovel
    Criação de variaveis e construtor

--- Java11/src/br/desafio/models/Imovel.java
+++ Java11/src/br/desafio/models/Imovel.java
@@ -2,5 +2,23 @@ package br.desafio.models;
 
 public class Imovel {
 
+    private int Id;
+    private String nome;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String fachada;
+    private String rua;
+    private String planta;
 
+    public Imovel(int id, String nome, String bairro, String cep, String cidade, String fachada, String rua, String planta) {
+        Id = id;
+        this.nome = nome;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.fachada = fachada;
+        this.rua = rua;
+        this.planta = planta;
+    }
 }
