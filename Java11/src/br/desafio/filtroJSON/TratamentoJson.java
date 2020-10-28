70d26500f999ff5d5e43247b54286122080e5852
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-27 22:26:55 -0300

    Estruturação
    Estruturação da classe TratamentoJson

--- Java11/src/br/desafio/filtroJSON/TratamentoJson.java
+++ Java11/src/br/desafio/filtroJSON/TratamentoJson.java
@@ -1,10 +1,22 @@
 package br.desafio.filtroJSON;
 
-import org.json.JSONObject;
+import br.desafio.API.ImobiliariaAPI;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class TratamentoJson {
+    ImobiliariaAPI imobiliariaAPI = new ImobiliariaAPI();
 
-    public JSONObject sortCidade(JSONObject jsonObject){
+    JSONArray jsonArrayImoveis;
 
+    {
+        try {
+            jsonArrayImoveis = imobiliariaAPI.imoveisJsonArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
