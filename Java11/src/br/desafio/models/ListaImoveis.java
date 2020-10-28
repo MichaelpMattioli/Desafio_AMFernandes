beff09f2a518020f81b128d54c87a54f331ffff8
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-28 16:06:15 -0300

    Criação de lista de imovel

---
+++ Java11/src/br/desafio/models/ListaImoveis.java
@@ -0,0 +1,11 @@
+package br.desafio.models;
+
+import java.util.List;
+
+public class ListaImoveis {
+    private List<Imovel> listaImoveis;
+
+    public ListaImoveis(List<Imovel> listaImoveis) {
+        this.listaImoveis = listaImoveis;
+    }
+}
