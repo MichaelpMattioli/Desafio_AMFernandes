dd960d855d263245701c7d11874ee10606d0092c
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-29 00:41:28 -0300

    Estruturando a classe imovel

--- Java11/src/br/desafio/models/Imovel.java
+++ Java11/src/br/desafio/models/Imovel.java
@@ -2,56 +2,11 @@ package br.desafio.models;
 
 public class Imovel {
 
-    private int Id;
     private String nome;
     private String bairro;
     private String cidade;
     private String fachada;
     private String rua;
-
-    public Imovel(int id, String nome, String bairro, String cidade, String fachada, String rua) {
-        Id = id;
-        this.nome = nome;
-        this.bairro = bairro;
-        this.cidade = cidade;
-        this.fachada = fachada;
-        this.rua = rua;
-    }
-
-    public int getId() {
-        return Id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public String getFachada() {
-        return fachada;
-    }
-
-    public String getRua() {
-        return rua;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Imovel{" +
-                "Id=" + Id +
-                ", nome='" + nome + '\'' +
-                ", bairro='" + bairro + '\'' +
-                ", cidade='" + cidade + '\'' +
-                ", fachada='" + fachada + '\'' +
-                ", rua='" + rua + '\'' +
-                '}';
-    }
+    private String num;
+    private String cep;
 }
