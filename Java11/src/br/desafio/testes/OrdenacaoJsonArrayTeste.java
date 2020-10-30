258639e64a8489d2d829240a199fdb04d795a8d7
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-30 16:58:22 -0300

    Teste de funcionalidade

--- Java11/src/br/desafio/testes/OrdenacaoJsonArrayTeste.java
+++ Java11/src/br/desafio/testes/OrdenacaoJsonArrayTeste.java
@@ -1,14 +1,17 @@
 package br.desafio.testes;
 
-import br.desafio.tratamentosJson.OrdenacaoJsonArray;
+import br.desafio.tratamentosJson.InfoCamposJsonArray;
 
 public class OrdenacaoJsonArrayTeste {
 
     public void run(){
-        OrdenacaoJsonArray ordenacaoJsonArray = new OrdenacaoJsonArray();
+        InfoCamposJsonArray infoCamposJsonArray = new InfoCamposJsonArray();
 
-        System.out.println(ordenacaoJsonArray.jsonArraySortCamposAlfabetic("nome"));
+//        System.out.println(ordenacaoJsonArray.jsonArraySortCamposAlfabetic("nome"));
 
 //        System.out.println(ordenacaoJsonArray.jsonArraySortCamposNumber(0, "planta", "preco"));
+
+        System.out.println(infoCamposJsonArray.jsonArraySortCamposNumberRange(3.0,3.0, 1, "planta", "vagas"));
+
     }
 }
