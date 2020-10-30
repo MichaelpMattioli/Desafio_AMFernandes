8626362c2f87960aa5eba5bf557e03faaec76cae
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-30 17:53:09 -0300

    Alguns testes de funcionalidades de filtros
    Testando se o filtro de arraysJS filtrados está funcionando.

--- Java11/src/br/desafio/testes/OrdenacaoJsonArrayTeste.java
+++ Java11/src/br/desafio/testes/OrdenacaoJsonArrayTeste.java
@@ -1,17 +1,42 @@
 package br.desafio.testes;
 
+import br.desafio.API.ImobiliariaAPI;
 import br.desafio.tratamentosJson.OrdenacaoJsonArray;
+import org.json.JSONArray;
 
 public class OrdenacaoJsonArrayTeste {
 
     public void run(){
         OrdenacaoJsonArray ordenacaoJsonArray = new OrdenacaoJsonArray();
 
+        ImobiliariaAPI imobiliariaAPI = new ImobiliariaAPI();
+
+        JSONArray jsonArrayImoveis; // 587 imoveis
+
+        {
+            try {
+                jsonArrayImoveis = imobiliariaAPI.imoveisJsonArray();
+            } catch (Exception e) {
+                jsonArrayImoveis = new JSONArray();
+            }
+        }
+
 //        System.out.println(ordenacaoJsonArray.jsonArraySortCamposAlfabetic("nome"));
 
 //        System.out.println(ordenacaoJsonArray.jsonArraySortCamposNumber(0, "planta", "preco"));
 
-        System.out.println(ordenacaoJsonArray.jsonArraySortCamposNumberRange(3.0,3.0, 1, "planta", "vagas"));
+//        System.out.println(ordenacaoJsonArray.jsonArraySortCamposNumberRange(3.0,3.0, 1, "planta", "vagas"));
+
+//        System.out.println(ordenacaoJsonArray.jsonArrayImoveisFiltrados(jsonArrayImoveis ,"Santo André", "cidade", null));
+
+//        JSONArray jsonArray = ordenacaoJsonArray.jsonArrayImoveisFiltrados(jsonArrayImoveis ,"Santo André", "cidade", null);
+        JSONArray jsonArray = ordenacaoJsonArray.jsonArrayImoveisFiltrados(jsonArrayImoveis ,"Santo Antônio", "bairro", null);
+//        JSONArray jsonArray = ordenacaoJsonArray.jsonArrayImoveisFiltrados(jsonArrayImoveis ,"São Bernardo do Campo", "cidade", null);
+
+        JSONArray jsonArray1 = ordenacaoJsonArray.jsonArraySortCamposNumberRange(jsonArray, 0.0 , 1.0, 0, "planta", "vagas" );
+
+        System.out.println(jsonArray1);
+
 
     }
 }
