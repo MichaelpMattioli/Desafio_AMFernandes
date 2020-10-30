24f5ce5d172be7f5197cc43a54a82f6b3ae13a7c
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-30 04:47:12 -0300

    Refatoração da main
    Responsável agora por inicializar o metodo run da classe de testes de ordenacaoJsonArray

--- Java11/src/br/desafio/Main.java
+++ Java11/src/br/desafio/Main.java
@@ -1,11 +1,15 @@
 package br.desafio;
 
+import br.desafio.testes.OrdenacaoJsonArrayTeste;
 import br.desafio.testes.TratamentoJsonTeste;
 
 public class Main {
 
     public static void main(String[] args) {
-        TratamentoJsonTeste tratamentoJsonTeste = new TratamentoJsonTeste();
-        tratamentoJsonTeste.run();
+//        TratamentoJsonTeste tratamentoJsonTeste = new TratamentoJsonTeste();
+//        tratamentoJsonTeste.run();
+
+        OrdenacaoJsonArrayTeste ordenacaoJsonArrayTeste = new OrdenacaoJsonArrayTeste();
+        ordenacaoJsonArrayTeste.run();
     }
 }
