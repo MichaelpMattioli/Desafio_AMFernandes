9ec71db480d05fcb9e6a55453a391c8ec2767fbb
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-31 04:33:16 -0300

    Teste da classe ImovelJS

--- Java11/src/br/desafio/testes/ImovelJSTeste.java
+++ Java11/src/br/desafio/testes/ImovelJSTeste.java
@@ -1,12 +1,16 @@
 package br.desafio.testes;
 
+import br.desafio.ImobiliariaAPI;
 import br.desafio.JS.ImovelJS;
 
 public class ImovelJSTeste {
 
     ImovelJS imovelJS = new ImovelJS();
+    ImobiliariaAPI imobiliariaAPI = new ImobiliariaAPI();
 
     public void run(){
-        System.out.println(imovelJS.imoveisListSortCamposNumberRange(0.0 , 1.0, 0, "planta", "vagas" ));
+//        System.out.println(imovelJS.imoveisListSortCamposNumberRange(0.0 , 1.0, 0, "planta", "vagas" ));
+        System.out.println(imobiliariaAPI.imoveisJsonArray());
+        System.out.println(imovelJS.imovelListPuro());
     }
 }
