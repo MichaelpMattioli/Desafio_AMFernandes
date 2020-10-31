46bcbb2fcf4b28af2fc00f4a9e94b147a73294ff
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-31 01:39:19 -0300

    Retirada de imports da main

--- Java11/src/br/desafio/Main.java
+++ Java11/src/br/desafio/Main.java
@@ -1,12 +1,10 @@
 package br.desafio;
 
-import br.desafio.models.tratamentosJson.OrdenacaoJsonArray;
 import br.desafio.testes.OrdenacaoJsonArrayTeste;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
