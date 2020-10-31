2f0b3dd446eb3db32f6161941e0233293b296646
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-31 02:06:17 -0300

    Estruturação da classe ListaImoveis
    Adição de método importantes para navegar na lista

--- Java11/src/br/desafio/models/ListaImoveis.java
+++ Java11/src/br/desafio/models/ListaImoveis.java
@@ -3,9 +3,35 @@ package br.desafio.models;
 import java.util.List;
 
 public class ListaImoveis {
-    private List<Imovel> listaImoveis;
+    private List<Imovel> listImovel;
+    private int imovelAtual;
 
-    public ListaImoveis(List<Imovel> listaImoveis) {
-        this.listaImoveis = listaImoveis;
+    public ListaImoveis(List<Imovel> listImovel) {
+        this.listImovel = listImovel;
+        this.imovelAtual = 0;
+    }
+
+    public final Imovel getlistImovelAtual(){
+        return listImovel.get(imovelAtual);
+    }
+
+    public boolean switchToPreviousCard(){
+        if(imovelAtual > 0){
+            imovelAtual--;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean switchToNextCard(){
+        if(imovelAtual < listImovel.size() - 1){
+            imovelAtual++;
+            return true;
+        }
+        return false;
+    }
+
+    public int getImovelAtual() {
+        return imovelAtual;
     }
 }
