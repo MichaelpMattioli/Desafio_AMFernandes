1a7ace9b0ebef7ede64a75f024c0bda929d68e0e
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-31 01:32:21 -0300

    Update module-info.java

--- Java11/src/module-info.java
+++ Java11/src/module-info.java
@@ -1,8 +1,8 @@
 module Java11 {
+    requires org.json;
     requires java.net.http;
     requires javafx.fxml;
     requires javafx.controls;
-    requires org.json;
     requires sqlite.jdbc;
 
     opens br.desafio;
