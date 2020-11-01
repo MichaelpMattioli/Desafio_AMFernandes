559dbdaa2d8c77e5971b3c00720860bc49e9db22
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-31 21:32:16 -0300

    Desativação de classe InfoCamposJsonArrayTeste

--- Java11/src/br/desafio/testes/InfoCamposJsonArrayTeste.java
+++ Java11/src/br/desafio/testes/InfoCamposJsonArrayTeste.java
@@ -1,62 +1,62 @@
-package br.desafio.testes;
-
-import br.desafio.ImobiliariaAPI;
-import br.desafio.sorts.Sorts;
-import br.desafio.JS.tratamentosJson.InfoCamposJsonArray;
-import org.json.JSONArray;
-
-import java.util.ArrayList;
-
-public class InfoCamposJsonArrayTeste {
-
-    public void run(){
-
-        ImobiliariaAPI imobiliariaAPI = new ImobiliariaAPI();
-
-        JSONArray jsonArrayImoveis = null; // 587 imoveis
-
-        {
-            try {
-                jsonArrayImoveis = imobiliariaAPI.imoveisJsonArray();
-            } catch (Exception e) {
-                return;
-            }
-        }
-
-        String campo = "cidade";
-        String subCampo = null;
-        String valorDeCampo = "São Caetano do Sul";
-
-        InfoCamposJsonArray infoCamposJsonArray = new InfoCamposJsonArray();
-
-        System.out.println(jsonArrayImoveis);
-
-        ArrayList arrayListInfoCamposSemRepeticao = infoCamposJsonArray.arrayListInfoCamposSemRepeticao(jsonArrayImoveis,campo,subCampo);
-
-        System.out.println(arrayListInfoCamposSemRepeticao);
-
-        ArrayList arrayList = (ArrayList) infoCamposJsonArray.arrayListInfoCamposSemRepeticao(jsonArrayImoveis,campo,subCampo).get(arrayListInfoCamposSemRepeticao.size()-1);
-
-        System.out.println(arrayList);
-        System.out.println("Size: " + arrayList.size());
-
-        // SORT
-
-        Sorts sorts = new Sorts();
-
-        // sort Alfanumerico
+//package br.desafio.testes;
+//
+//import br.desafio.ImobiliariaAPI;
+//import br.desafio.sorts.Sorts;
+//import br.desafio.JS.tratamentosJson.InfoCamposJsonArray;
+//import org.json.JSONArray;
+//
+//import java.util.ArrayList;
+//
+//public class InfoCamposJsonArrayTeste {
+//
+//    public void run(){
+//
+//        ImobiliariaAPI imobiliariaAPI = new ImobiliariaAPI();
+//
+//        JSONArray jsonArrayImoveis = null; // 587 imoveis
+//
+//        {
+//            try {
+//                jsonArrayImoveis = imobiliariaAPI.imoveisJsonArray();
+//            } catch (Exception e) {
+//                return;
+//            }
+//        }
+//
+//        String campo = "cidade";
+//        String subCampo = null;
+//        String valorDeCampo = "São Caetano do Sul";
+//
+//        InfoCamposJsonArray infoCamposJsonArray = new InfoCamposJsonArray();
+//
+//        System.out.println(jsonArrayImoveis);
+//
+//        ArrayList arrayListInfoCamposSemRepeticao = infoCamposJsonArray.arrayListInfoCamposSemRepeticao(jsonArrayImoveis,campo,subCampo);
+//
+//        System.out.println(arrayListInfoCamposSemRepeticao);
+//
+//        ArrayList arrayList = (ArrayList) infoCamposJsonArray.arrayListInfoCamposSemRepeticao(jsonArrayImoveis,campo,subCampo).get(arrayListInfoCamposSemRepeticao.size()-1);
+//
+//        System.out.println(arrayList);
+//        System.out.println("Size: " + arrayList.size());
+//
+//        // SORT
+//
+//        Sorts sorts = new Sorts();
+//
+//         sort Alfanumerico
 //        System.out.println(sorts.sortStringOrdemAlfabetica(arrayList));
 //        System.out.println(sorts.sortStringOrdemAlfabetica(arrayList).size());
-
-
-        // sort Numero
+//
+//
+//         sort Numero
 //        System.out.println(sorts.sortNumber(0, arrayList));
 //        System.out.println(sorts.sortNumber(0,arrayList).size());
-
-        // Filtro
-
+//
+//         Filtro
+//
 //        System.out.println(tratamentoJson.jsonArrayImoveisFiltrados(jsonArrayImoveis, valorDeCampo, campo,subCampo));
 //        System.out.println(tratamentoJson.jsonArrayImoveisFiltrados(jsonArrayImoveis, valorDeCampo, campo,subCampo).length());
-
-    }
-}
+//
+//    }
+//}
