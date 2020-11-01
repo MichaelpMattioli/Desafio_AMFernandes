1ad22a14fc5a5e17c702fb885fb95c695c3edac7
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-10-31 21:27:15 -0300

    Tratamento de API em caso o GET não funcione

--- Java11/src/br/desafio/ImobiliariaAPI.java
+++ Java11/src/br/desafio/ImobiliariaAPI.java
@@ -8,16 +8,26 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ImobiliariaAPI {
-    public JSONArray imoveisJsonArray() throws Exception{
-        HttpClient client = HttpClient.newBuilder().build();
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("https://api.estagio.amfernandes.com.br/imoveis"))
-                .build();
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
-        JSONArray jsonArray = new JSONArray(response.body());
+    public JSONArray imoveisJsonArray() {
 
-        return jsonArray;
+        try{
+            java.util.concurrent.TimeUnit.SECONDS.sleep(2);
+            HttpClient client = HttpClient.newBuilder().build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create("https://api.estagio.amfernandes.com.br/imoveis"))
+                    .build();
+            HttpResponse<String> response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+            JSONArray jsonArray = new JSONArray(response.body());
+
+            return jsonArray;
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("NAO FOI POSSIVEL ACESSAR A API!");
+        }
+
+        return null;
+
     }
 }
