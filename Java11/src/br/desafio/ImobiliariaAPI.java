35d03a2e362bbacf07438170592d1395786d7dd6
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-11-02 21:12:07 -0300

    Documentação da requesição da API

--- Java11/src/br/desafio/ImobiliariaAPI.java
+++ Java11/src/br/desafio/ImobiliariaAPI.java
@@ -7,7 +7,19 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+/**
+ * Classe responsável por fazer a requisição da API pelo link fornecido pelo desafio.
+ *  * @author Michael Pedroza Mattioli Leite - michael.pmattioli@gmail.com
+ *  * @since 02/11/2020
+ *  * @version 1.0
+ */
+
 public class ImobiliariaAPI {
+
+    /**
+     * Método responsável por fazer a requisição da API pelo link fornecido pelo desafio.
+     * @return Retorna um Array de JSON com as informações adquiridas pelo link.
+     */
     public JSONArray imoveisJsonArray() {
 
         try{
