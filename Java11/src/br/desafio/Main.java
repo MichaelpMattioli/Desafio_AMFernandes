0030ec3618c443f8cf252fb9474d9d890de1d390
Michael Pedroza Mattioli Leite <michael.pmattioli@gmail.com>
2020-11-02 23:16:32 -0300

    Documentação Main

--- Java11/src/br/desafio/Main.java
+++ Java11/src/br/desafio/Main.java
@@ -6,9 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+/**
+ * Classe Main, responsável por inicializar o programa.
+ *  * @author Michael Pedroza Mattioli Leite - michael.pmattioli@gmail.com
+ *  * @since 02/11/2020
+ *  * @version 1.0
+ */
 
-    ControllerInterface controllerInterface = new ControllerInterface();
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -16,7 +21,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        controllerInterface.initActions();
         Parent root = FXMLLoader.load(getClass().getResource("ViewInterface.fxml"));
         primaryStage.setTitle("Desafio Estágio - AMFernantes");
         primaryStage.setScene(new Scene(root, 1038, 600));
